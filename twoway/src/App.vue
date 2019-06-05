e840fb7a74de33150b40319252b60f4eaff4640f
zy499 <hqh499@gmail.com>
2019-06-06 01:36:41 +0800

    up

--- twoway/src/App.vue
+++ twoway/src/App.vue
@@ -1,107 +1,108 @@
 <template>
-  <div>
+<div>
     <!-- 自定义Input实现双向绑定 -->
     <h2>自定义Input实现双向绑定</h2>
     {{value}}
     <k-input :value="value" @input="value = arguments[0]" />
     <h2>element表单</h2>
-    <el-form
-      :model="ruleForm2"
-      status-icon
-      :rules="rules2"
-      ref="ruleForm2"
-      label-width="100px"
-      class="demo-ruleForm"
-      :style="{ width:'500px'}"
-    >
-      <el-form-item label="密码" prop="pass">
-        <el-input type="password" v-model="ruleForm2.pass" autocomplete="off"></el-input>
-      </el-form-item>
-      <el-form-item label="确认密码" prop="checkPass">
-        <el-input type="password" v-model="ruleForm2.checkPass" autocomplete="off"></el-input>
-      </el-form-item>
-      <el-form-item label="年龄" prop="age">
-        <el-input v-model.number="ruleForm2.age"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
-        <el-button @click="resetForm('ruleForm2')">重置</el-button>
-      </el-form-item>
+    <el-form :model="ruleForm2" status-icon :rules="rules2" ref="ruleForm2" label-width="100px" class="demo-ruleForm" :style="{ width:'500px'}">
+        <el-form-item label="密码" prop="pass">
+            <el-input type="password" v-model="ruleForm2.pass" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="checkPass">
+            <el-input type="password" v-model="ruleForm2.checkPass" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="年龄" prop="age">
+            <el-input v-model.number="ruleForm2.age"></el-input>
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
+            <el-button @click="resetForm('ruleForm2')">重置</el-button>
+        </el-form-item>
     </el-form>
-  </div>
+</div>
 </template>
 
 <script>
 import KInput from './components/Input'
 export default {
-  name: "app",
-  components: {
-    KInput
-  },
-  data() {
-    var checkAge = (rule, value, callback) => {
-      if (!value) {
-        return callback(new Error("年龄不能为空"));
-      }
-      setTimeout(() => {
-        if (!Number.isInteger(value)) {
-          callback(new Error("请输入数字值"));
-        } else {
-          if (value < 18) {
-            callback(new Error("必须年满18岁"));
-          } else {
-            callback();
-          }
-        }
-      }, 1000);
-    };
-    var validatePass = (rule, value, callback) => {
-      if (value === "") {
-        callback(new Error("请输入密码"));
-      } else {
-        if (this.ruleForm2.checkPass !== "") {
-          this.$refs.ruleForm2.validateField("checkPass");
-        }
-        callback();
-      }
-    };
-    var validatePass2 = (rule, value, callback) => {
-      if (value === "") {
-        callback(new Error("请再次输入密码"));
-      } else if (value !== this.ruleForm2.pass) {
-        callback(new Error("两次输入密码不一致!"));
-      } else {
-        callback();
-      }
-    };
-    return {
-      value:"",
-      ruleForm2: {
-        pass: "",
-        checkPass: "",
-        age: ""
-      },
-      rules2: {
-        pass: [{ validator: validatePass, trigger: "blur" }],
-        checkPass: [{ validator: validatePass2, trigger: "blur" }],
-        age: [{ validator: checkAge, trigger: "blur" }]
-      }
-    };
-  },
-  methods: {
-    submitForm(formName) {
-      this.$refs[formName].validate(valid => {
-        if (valid) {
-          alert("submit!");
-        } else {
-          return false;
-        }
-      });
+    name: "app",
+    components: {
+        KInput
     },
-    resetForm(formName) {
-      this.$refs[formName].resetFields();
+    data() {
+        var checkAge = (rule, value, callback) => {
+            if (!value) {
+                return callback(new Error("年龄不能为空"));
+            }
+            setTimeout(() => {
+                if (!Number.isInteger(value)) {
+                    callback(new Error("请输入数字值"));
+                } else {
+                    if (value < 18) {
+                        callback(new Error("必须年满18岁"));
+                    } else {
+                        callback();
+                    }
+                }
+            }, 1000);
+        };
+        var validatePass = (rule, value, callback) => {
+            if (value === "") {
+                callback(new Error("请输入密码"));
+            } else {
+                if (this.ruleForm2.checkPass !== "") {
+                    this.$refs.ruleForm2.validateField("checkPass");
+                }
+                callback();
+            }
+        };
+        var validatePass2 = (rule, value, callback) => {
+            if (value === "") {
+                callback(new Error("请再次输入密码"));
+            } else if (value !== this.ruleForm2.pass) {
+                callback(new Error("两次输入密码不一致!"));
+            } else {
+                callback();
+            }
+        };
+        return {
+            value: "",
+            ruleForm2: {
+                pass: "",
+                checkPass: "",
+                age: ""
+            },
+            rules2: {
+                pass: [{
+                    validator: validatePass,
+                    trigger: "blur"
+                }],
+                checkPass: [{
+                    validator: validatePass2,
+                    trigger: "blur"
+                }],
+                age: [{
+                    validator: checkAge,
+                    trigger: "blur"
+                }]
+            }
+        };
+    },
+    methods: {
+        submitForm(formName) {
+            this.$refs[formName].validate(valid => {
+                if (valid) {
+                    alert("submit!");
+                } else {
+                    return false;
+                }
+            });
+        },
+        resetForm(formName) {
+            this.$refs[formName].resetFields();
+        }
     }
-  }
 };
 </script>
 
