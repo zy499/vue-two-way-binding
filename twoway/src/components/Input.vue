73b4e8469119fc7bc023fee12d24f4326552e2c5
zy499 <hqh499@gmail.com>
2019-06-06 01:43:53 +0800

    up

--- twoway/src/components/Input.vue
+++ twoway/src/components/Input.vue
@@ -1,8 +1,8 @@
 <!-- 自定义实现双向绑定 -->
 <template>
-  <div class>
-      <input type="text" :value="value" @input="onInput">
-  </div>
+    <div class>
+        <input type="text" :value="value" @input="onInput" />
+    </div>
 </template>
 
 <script>
@@ -10,36 +10,37 @@
 //例如：import 《组件名称》 from '《组件路径》';
 
 export default {
-  //import引入的组件需要注入到对象中才能使用
-  props: ['value'],
-  components: {},
-  data() {
-    //这里存放数据
-    return {};
-  },
-  //监听属性 类似于data概念
-  computed: {},
-  //监控data中的数据变化
-  watch: {},
-  //方法集合
-  methods: {
-      onInput(e){
-          let value = e.target.value
-          this.$emit('input',value)
-      }
-  },
-  //生命周期 - 创建完成（可以访问当前this实例）
-  created() {},
-  //生命周期 - 挂载完成（可以访问DOM元素）
-  mounted() {},
-  beforeCreate() {}, //生命周期 - 创建之前
-  beforeMount() {}, //生命周期 - 挂载之前
-  beforeUpdate() {}, //生命周期 - 更新之前
-  updated() {}, //生命周期 - 更新之后
-  beforeDestroy() {}, //生命周期 - 销毁之前
-  destroyed() {}, //生命周期 - 销毁完成
-  activated() {} //如果页面有keep-alive缓存功能，这个函数会触发
+    //import引入的组件需要注入到对象中才能使用
+    props: ['value'],
+    components: {},
+    data() {
+        //这里存放数据
+        return {};
+    },
+    //监听属性 类似于data概念
+    computed: {},
+    //监控data中的数据变化
+    watch: {},
+    //方法集合
+    methods: {
+        onInput(e) {
+            let value = e.target.value
+            this.$emit('input', value)
+        }
+    },
+    //生命周期 - 创建完成（可以访问当前this实例）
+    created() { },
+    //生命周期 - 挂载完成（可以访问DOM元素）
+    mounted() { },
+    beforeCreate() { }, //生命周期 - 创建之前
+    beforeMount() { }, //生命周期 - 挂载之前
+    beforeUpdate() { }, //生命周期 - 更新之前
+    updated() { }, //生命周期 - 更新之后
+    beforeDestroy() { }, //生命周期 - 销毁之前
+    destroyed() { }, //生命周期 - 销毁完成
+    activated() { } //如果页面有keep-alive缓存功能，这个函数会触发
 };
 </script>
+
 <style scoped>
 </style>
